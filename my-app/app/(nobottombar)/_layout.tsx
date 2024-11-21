b6defadc3bce4246bdf4daa574b8aa9beb7a0eab
Urcodingbuddy <anshpethe110@gmail.com>
2024-11-21 14:07:13 +0530

    stack navigation with 1 error

--- my-app/app/(nobottombar)/_layout.tsx
+++ my-app/app/(nobottombar)/_layout.tsx
@@ -14,6 +14,7 @@ export default function Layout() {
                     headerBackTitle: "Go-back",
                 }}
             />
+            
             <Slot />
         </Stack>
     );
